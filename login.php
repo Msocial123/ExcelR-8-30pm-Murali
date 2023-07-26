1b439f5b4e32eae2d855f07a548d1d3279c55470
Raj Kumar <rajkumar2022@gmail.com>
2023-07-26 15:33:03 +0000

    Changes happend from onshore team

--- login.php
+++ login.php
@@ -2,7 +2,9 @@
     $host = "localhost";  
     $user = "root";  
     $password = '';  
-    $db_name = "javatpoint";  
+    $db_name = "javatpoint";
+    $db_username="Murali"
+    $db_password="Mur@li2022"	    
       
     $con = mysqli_connect($host, $user, $password, $db_name);  
     if(mysqli_connect_errno()) {  
